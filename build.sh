42f00debc87c94ed35eb1001023350846e56b4c2
Oli Zimpasser <oglimmer@gmail.com>
2025-01-26 13:51:33 +0100

    Build scraper image

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -eu
 
 docker build --build-arg API_URL=https://api-news.oglimmer.com  --tag registry.oglimmer.com/news-frontend:latest news-frontend
 docker build --tag registry.oglimmer.com/news-backend:latest news-backend
+docker build --tag registry.oglimmer.com/news-scraper:latest scraper
 
 docker push registry.oglimmer.com/news-frontend:latest
 docker push registry.oglimmer.com/news-backend:latest
+docker push registry.oglimmer.com/news-scraper:latest
